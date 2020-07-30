2bc7f9713b1fe6a4bfbd5f61d2567b3dba4da404
zebestlove <zbyslav13@gmail.com>
2020-07-30 10:46:03 +0300

    changing oper in display

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const display = document.querySelector('.display');
 
+
 document.querySelectorAll('.digits button')
     .forEach( button => button.addEventListener('click', digitPressed));
 
@@ -8,13 +9,25 @@ function digitPressed(ev) {
     display.value += digit;
 }
 
+
 document.querySelectorAll('.opers button')
     .forEach( button => button.addEventListener('click', operPressed));
 
 function operPressed(ev) {
     const oper = ev.target.innerText;
+    if (display.value.indexOf("+") > -1 || display.value.indexOf("-") > -1 || display.value.indexOf("*") > -1 || display.value.indexOf("/") > -1){
+        display.value = display.value.substring(0, display.value.length - 1);
+    }
     display.value += oper;
 }
+
+
+document.querySelector('.erase').addEventListener('click', erasePressed);
+    
+function erasePressed() {
+    display.value = "";
+}
+
 
 document.querySelector('.eq').addEventListener('click', eqPressed);
 
