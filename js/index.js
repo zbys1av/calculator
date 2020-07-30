3fec35b948b5f48148f44519b2ea0f3b36242b59
zebestlove <zbyslav13@gmail.com>
2020-07-30 20:14:41 +0300

    changing oper

--- js/index.js
+++ js/index.js
@@ -1,57 +1,84 @@
 const display = document.querySelector('.display');
+const historyDisplay = document.querySelector('.historyDisplay');
 
-
-document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+document
+  .querySelectorAll('.digits button')
+  .forEach((button) => button.addEventListener('click', digitPressed));
 
 function digitPressed(ev) {
-    const digit = ev.target.innerText;
-    display.value += digit;
+  const digit = ev.target.innerText;
+  display.value += digit;
 }
 
 document.querySelector('.point').addEventListener('click', pointPressed);
 
-function pointPressed(){
-    //const point = ev.target.innerText;
-    if (display.value == 0){
-        display.value += "0.";
-    } else {
-        display.value += ".";
-    }
+function pointPressed() {
+  //const point = ev.target.innerText;
+  if (display.value == 0) {
+    display.value += '0.';
+  } else {
+    display.value += '.';
+  }
 }
 
-
-document.querySelectorAll('.opers button')
-    .forEach( button => button.addEventListener('click', operPressed));
+document
+  .querySelectorAll('.opers button')
+  .forEach((button) => button.addEventListener('click', operPressed));
 
 function operPressed(ev) {
-    const oper = ev.target.innerText;
-    if (display.value.indexOf("+") > -1 || display.value.indexOf("-") > -1 || display.value.indexOf("*") > -1 || display.value.indexOf("/") > -1){
-        if (display.value.indexOf(oper) == (display.value.length)+1){
-            display.value = display.value.substring(0, display.value.length - 1);
-        }
-        eqPressed();
+  //let newOper;
+  const oper = ev.target.innerText;
+  for (let i = 0; i < 1; i++) {
+    if (
+      display.value.indexOf(oper)
+      // display.value.indexOf('-') > -1 ||
+      // display.value.indexOf('*') > -1 ||
+      // display.value.indexOf('/') > -1
+    ) {
+      //newOper = ev.target.innerText;
+      // if (display.value.indexOf(oper) <= (display.value.length)+1){
+      //display.value += display.value.substring(0, display.value.length - 1);
+      //}
+      display.value = display.value.split('+').join('');
+      display.value = display.value.split('*').join('');
+      display.value = display.value.split('-').join('');
+      display.value = display.value.split('/').join('');
+      //eqPressed();
     }
     display.value += oper;
+  }
 }
-
 
 document.querySelector('.erase').addEventListener('click', erasePressed);
-    
+
 function erasePressed() {
-    display.value = "";
+  display.value = '';
 }
 
-document.querySelector('.erase-last-element').addEventListener('click', eraseLastPressed);
+document
+  .querySelector('.erase-last-element')
+  .addEventListener('click', eraseLastPressed);
 
-function eraseLastPressed(){
-    display.value = display.value.substring(0, display.value.length - 1);
+function eraseLastPressed() {
+  display.value = display.value.substring(0, display.value.length - 1);
 }
-
 
 document.querySelector('.eq').addEventListener('click', eqPressed);
 
 function eqPressed() {
-    //display.value = Math.round((eval(display.value) + Number.EPSILON) * 100) / 100;
-    display.value = (eval(display.value)).toFixed(2);
+  //display.value = Math.round((eval(display.value) + Number.EPSILON) * 100) / 100;
+  historyDisplay.value += display.value;
+  let divisionZero = 'Division cannot be zero';
+  if (eval(display.value).toFixed(2) == Infinity) {
+    display.value = divisionZero;
+  } else {
+    display.value = eval(display.value).toFixed(2);
+  }
+  historyDisplay.value += '=' + display.value + ' ';
+}
+
+document.querySelector('.history').addEventListener('click', historyPressed);
+
+function historyPressed() {
+  historyDisplay.value = '';
 }
