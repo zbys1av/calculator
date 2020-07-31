073723a79f3df012dac56cffbfc78176779eb140
zebestlove <zbyslav13@gmail.com>
2020-07-31 12:49:14 +0300

    need to fix point and opers

--- js/index.js
+++ js/index.js
@@ -13,8 +13,7 @@ function digitPressed(ev) {
 document.querySelector('.point').addEventListener('click', pointPressed);
 
 function pointPressed() {
-  //const point = ev.target.innerText;
-  if (display.value == 0) {
+  if (display.value === '') {
     display.value += '0.';
   } else {
     display.value += '.';
@@ -26,33 +25,28 @@ document
   .forEach((button) => button.addEventListener('click', operPressed));
 
 function operPressed(ev) {
-  //let newOper;
   const oper = ev.target.innerText;
   for (let i = 0; i < 1; i++) {
-    if (
-      display.value.indexOf(oper)
-      // display.value.indexOf('-') > -1 ||
-      // display.value.indexOf('*') > -1 ||
-      // display.value.indexOf('/') > -1
-    ) {
-      //newOper = ev.target.innerText;
-      // if (display.value.indexOf(oper) <= (display.value.length)+1){
-      //display.value += display.value.substring(0, display.value.length - 1);
-      //}
+    if (display.value === '') {
+      if (oper === '+' || oper === '/' || oper === '*') {
+        display.value += '0' + oper;
+      }
+    }
+    if (display.value.indexOf(oper) === display.value.length + 1) {
+      eqPressed();
       display.value = display.value.split('+').join('');
       display.value = display.value.split('*').join('');
       display.value = display.value.split('-').join('');
       display.value = display.value.split('/').join('');
-      //eqPressed();
     }
-    display.value += oper;
   }
+  display.value += oper;
+  // }
 }
 
 document.querySelector('.percentage').addEventListener('click', percPressed);
 
 function percPressed(ev) {
-  // find some % of number
   digitPressed(ev);
 }
 
@@ -73,7 +67,6 @@ function eraseLastPressed() {
 document.querySelector('.eq').addEventListener('click', eqPressed);
 
 function eqPressed() {
-  //display.value = Math.round((eval(display.value) + Number.EPSILON) * 100) / 100;
   historyDisplay.value += display.value;
   let divisionZero = 'Division cannot be zero';
   if (display.value.indexOf('%') > -1) {
@@ -94,3 +87,6 @@ document.querySelector('.history').addEventListener('click', historyPressed);
 function historyPressed() {
   historyDisplay.value = '';
 }
+
+// changing all opers
+// point
