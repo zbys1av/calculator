96f96bca2939a2aaaa6fd581bd0fefdecd222fec
zebestlove <zbyslav13@gmail.com>
2020-08-17 13:10:28 +0300

    added themes, need to make them save in cash

--- js/index.js
+++ js/index.js
@@ -98,3 +98,15 @@ document.querySelector('.historyBtn').addEventListener('click', historyPressed);
 function historyPressed() {
   historyDisplay.value = '';
 }
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+  var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("type", "text/css");
+  newlink.setAttribute("href", cssFile);
+
+  document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
